8638ec7bd7bcadd7d520a9508248030496e6b375
Jelle Haasnoot <36537983+jellehaasnoot@users.noreply.github.com>
2018-02-16 15:56:03 +0100

    First minor changes to application

--- Application.py
+++ Application.py
@@ -1,28 +1,6 @@
 import wx as wx
 import os
 
-# class Main:
-#     """In this function a visualized version of the application will be created. In this application, the user
-#     can upload, review and save files which were created in the previous scripts"""
-#
-#
-#     #def __init__(self):
-#
-#
-#     # def application(self):
-#     #
-#     #     # The creation of the pop-up frame:
-#     #     visualized_app = wx.App(self)
-#     #     visualized_frame = wx.Frame(None, stringtitle="Hello world")
-#     #
-#     #     # The showing of the frame:
-#     #     visualized_frame.Show(self)
-#     #     # Create a panel in the frame:
-#     #     visualized_panel = wx.Panel(self)
-#     #
-#     #     # Start the event loop:
-#     #     visualized_app.MainLoop()
-
 class MyFrame(wx.Frame):
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, title=title, size=(500, 300))
@@ -31,7 +9,7 @@ class MyFrame(wx.Frame):
 
         # Create the file menu
         file_menu = wx.Menu()
-        
+
         menu_about = file_menu.Append(wx.ID_ABOUT, "&About", "Information about this program")
         file_menu.AppendSeparator()
         menu_file_open = file_menu.Append(wx.ID_FILE, "&Open file...", "Open a text file with this program")
@@ -55,7 +33,7 @@ class MyFrame(wx.Frame):
         if prompted_dialog.ShowModal() == wx.ID_OK:
             self.file_name = prompted_dialog.GetFilename()
             self.directory_name = prompted_dialog.GetDirectory()
-            file_to_open = open(os.path.join(self.directory_name, self.file_name, 'r'))
+            file_to_open = open(os.path.join(self.directory_name, self.file_name))
             file_to_open.close()
         prompted_dialog.Destroy()
 
@@ -67,7 +45,6 @@ class MyFrame(wx.Frame):
 
     def OnExit(self, e):
         self.Close(True)
-
 
 Application = wx.App(False)
 frame = MyFrame(None, 'Small editor')
